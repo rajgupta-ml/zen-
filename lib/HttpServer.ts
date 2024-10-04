8a590dd2f7bdb8f721d61cac954480214913850d
rajgupta-ml <pratikrajgupta23@gmail.com>
2024-10-04 13:36:28 +0530

    feat: Added the feauter for create all type of request

--- lib/HttpServer.ts
+++ lib/HttpServer.ts
@@ -6,20 +6,23 @@ import { TcpServer } from "./TcpServer";
 type IRouteHandlerCallback = (request: IHttpRequest, response: IHttpResponse) => void
 
 export class zen extends TcpServer {
-	private availableRoutes: Map<string, IRouteHandlerCallback>
+	private availableRoutes: { [Key: string]: Map<string, IRouteHandlerCallback> }
 	constructor() {
 		super()
-		this.availableRoutes = new Map()
+		this.availableRoutes = {
+			GET: new Map(),
+			POST: new Map(),
+			PUT: new Map(),
+			DELETE: new Map()
+		}
 	}
 	handleRequest(data: Buffer): string {
 		const request: IHttpRequest = this.parseRequest(data);
 		const response = new ZenResponse()
+		const handler = this.availableRoutes[request.method]?.get(request.url)
 		// Get request
-		if (request.method === "GET" && this.availableRoutes.has(request.url)) {
-			const handler = this.availableRoutes.get(request.url);
-			if (handler) {
-				handler(request, response);
-			}
+		if (handler) {
+			handler(request, response)
 		} else {
 			response.status(404).send('404 not found')
 		}
@@ -41,7 +44,19 @@ export class zen extends TcpServer {
 		return { method, url, headers, body }
 	}
 
+
+
 	get(url: string, cb: IRouteHandlerCallback) {
-		this.availableRoutes.set(url, cb)
+		this.availableRoutes.GET.set(url, cb)
 	}
+	post(url: string, cb: IRouteHandlerCallback) {
+		this.availableRoutes.POST.set(url, cb)
+	}
+	put(url: string, cb: IRouteHandlerCallback) {
+		this.availableRoutes.PUT.set(url, cb)
+	}
+	delete(url: string, cb: IRouteHandlerCallback) {
+		this.availableRoutes.DELETE.set(url, cb)
+	}
+
 }
